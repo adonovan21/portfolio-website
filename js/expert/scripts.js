49757eae0b65d6e79f60299445f4116e75ce8a89
adonovan21 <alex21donovan@gmail.com>
2024-05-25 21:10:02 -0500

    linter updates

--- js/expert/scripts.js
+++ js/expert/scripts.js
@@ -1,11 +1,7 @@
-//alert("Get ready to be amazed!");
-
-console.log("Welcome to my Portfolio Site");
-
 function menuToggle() {
     var x = document.getElementById('myNavtoggle');
     var isMobileBreakpoint = window.innerWidth <= 641
-    
+
     if (x.className === 'navtoggle' && isMobileBreakpoint) {
       x.className += ' responsive';
     } else {
